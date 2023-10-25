c7deac6964ca6785e871b07eee24be31a6a8d573
Ilshat Madiev <diamondplustree@gmail.com>
2023-10-25 17:03:04 +0500

    decomposition app.js & add server.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import breedsRouter from './routes/breedsRouter.js';
 import factsRouter from './routes/factsRouter.js';
 
-const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
@@ -10,8 +9,4 @@ app.use(express.json());
 app.use('/api', breedsRouter);
 app.use('/api', factsRouter);
 
-const server = app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
-
-module.exports = server;
+export default app
--- tests/test.test.js
+++ tests/test.test.js
@@ -1,16 +1,14 @@
 import request from "supertest";
 import app from "../src/app.js";
 
-afterAll((done) => {
-    app.close(() => {
-      console.log('Test server closed');
-      done();
-    });
-  });
 
-test('123', async () => {
+describe('GET /api/breeds', () => {
+  test('check standart response', async () => {
     const response = await request(app)
-        .get('/api/breeds')
+      .get('/api/breeds')
 
-    expect(response.status).toBe(200)
+    console.log(response.req);
+    expect(response.status).toBe(200);
+  })
 })
+
